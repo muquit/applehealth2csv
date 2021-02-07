286dd6d4b7857eb716797e7140d2d4a19bfb80d0
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-07 16:22:04 -0500

    Brew formula update for applehealth2csv version v1.0.1

--- Formula/applehealth2csv.rb
+++ Formula/applehealth2csv.rb
@@ -11,11 +11,11 @@ class Applehealth2csv < Formula
 
   if OS.mac?
     url "https://github.com/muquit/applehealth2csv/releases/download/v1.0.1/applehealth2csv_1.0.1_mac-64bit.tar.gz"
-    sha256 "7771463fcc5f90293b02cf7983153f9d8eb50e603a3079fe2ba614361147f5cb"
+    sha256 "3e6c97d524d12501d3f62440b72ea948cb40d1ec76497bcfed14fa2ea521b7a5"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/muquit/applehealth2csv/releases/download/v1.0.1/applehealth2csv_1.0.1_linux-64bit.tar.gz"
-    sha256 "3a0c8e91e205540dd128e6502988eeb361bdc0b033fa30242188700c640c2c10"
+    sha256 "e3d666ba8bc52ecc803c3168ba6355eec0f107fe39238ba9ecf24bc67b6ea6d6"
   end
 
   def install
